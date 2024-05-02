60c953ab04edb8c4bbafbb241cfb2b74bce088ed
let <let@notlet.dev>
2024-05-02 19:50:38 +0300

    fix broken spacing

--- ingameCommands/trophyfish.js
+++ ingameCommands/trophyfish.js
@@ -1,7 +1,7 @@
 const { number } = require('mathjs');
 const config = require('../config.json');
 const { getPlayer, numberformatter } = require('../helper/functions.js');
-const render = require('../helper/messageToImage.js');
+const render = require('../helper/loreRenderer.js').renderLore;
 const imgur = require('imgur-anonymous-uploader');
 
 const allTypes = {
@@ -13,11 +13,11 @@ const allTypes = {
 	flyfish: '§aFlyfish',
 	slugfish: '§aSlugfish',
 	obfuscated_fish_2: '§aObfuscated 2',
-	lava_horse: '§5Lavahorse',
-	mana_ray: '§5Mana Ray',
-	obfuscated_fish_3: '§5Obfuscated 3',
-	volcanic_stonefish: '§1Volcanic Stonefish',
-	vanille: '§1Vanille',
+	lava_horse: '§9Lavahorse',
+	mana_ray: '§9Mana Ray',
+	obfuscated_fish_3: '§9Obfuscated 3',
+	volcanic_stonefish: '§9Volcanic Stonefish',
+	vanille: '§9Vanille',
 	soul_fish: '§5Soul Fish',
 	karate_fish: '§5Karate Fish',
 	skeleton_fish: '§5Skeleton Fish',
@@ -45,7 +45,7 @@ module.exports = {
         
 		const total = numberformatter(trophyFish.total_caught, 1);
 
-		const maxLength = Math.max(...Object.values(allTypes).map(type => type.length)) - 2;
+		const maxLength = Math.max(...Object.values(allTypes).map(type => type.length));
 		const types = {};
 		Object.keys(allTypes).forEach(type => types[type] = [
 			trophyFish[type + '_bronze'] || 0,
@@ -56,19 +56,19 @@ module.exports = {
 
 		const formattedTypes = {};
 		Object.keys(types).forEach(type => formattedTypes[type] = [
-			types[type][0] > 0 ? '§8' + numberformatter(types[type][0], 1) : '§4×',
-			types[type][1] > 0 ? '§7' + numberformatter(types[type][1], 1) : '§4×',
-			types[type][2] > 0 ? '§6' + numberformatter(types[type][2], 1) : '§4×',
-			types[type][3] > 0 ? '§b' + numberformatter(types[type][3], 1) : '§4×',
+			types[type][0] > 0 ? '§8' + numberformatter(types[type][0], 1) : '§c×',
+			types[type][1] > 0 ? '§7' + numberformatter(types[type][1], 1) : '§c×',
+			types[type][2] > 0 ? '§6' + numberformatter(types[type][2], 1) : '§c×',
+			types[type][3] > 0 ? '§b' + numberformatter(types[type][3], 1) : '§c×',
 			`§f${numberformatter(types[type].reduce((a, b) => a + b), 1)}`
 		]);
 
-		const maxTypeLength = Math.max(...Object.values(formattedTypes).flat().map(n => n.length - 2));
-			
+		const maxTypeLength = Math.max(...Object.values(formattedTypes).flat().map(n => n.length));
 		const typeTable = Object.keys(types).map(type => `§7| ${allTypes[type].padStart(maxLength)} §7| ${formattedTypes[type].map(n => n.padStart(maxTypeLength)).join(' §7| ')} §7|`);
 
-		const rendered = render(`§6§l${username}'s Trophy Fish\n\n§fTotal Caught: §l${total}\n§7${'-'.repeat(typeTable[0].length - 16)}\n${typeTable.join('\n')}\n§7${'-'.repeat(typeTable[0].length - 16)}`, null, true);
+		const rendered = await render(`§6§l${username}'s Trophy Fish`, `§fTotal Caught: §l${total}\n§7${'='.repeat(typeTable[0].length - 26)}\n${typeTable.join('\n')}\n§7${'='.repeat(typeTable[0].length - 26)}`.split('\n'));
 
+		require('fs').writeFileSync('./trophyfish.png', rendered);
 		const uploadResponse = await uploader.uploadBuffer(rendered);
 		if (!uploadResponse.url) return minecraftClient.chat(`/gc @${messageAuthor} Failed to upload image.`);
 
